41adbc00856a731e86674d603e7dd02465fec873
Ben Mohammad <benmohammad593@gmail.com>
2020-02-22 11:43:09 +0000

    AddEditTaskInjector added...

---
+++ app/src/main/java/com/benmohammad/mobiusapplication/addedittask/AddEditTaskInjector.java
@@ -0,0 +1,32 @@
+package com.benmohammad.mobiusapplication.addedittask;
+
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEffect;
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEvent;
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskLogic;
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskMode;
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskModel;
+import com.spotify.mobius.MobiusLoop;
+import com.spotify.mobius.android.AndroidLogger;
+import com.spotify.mobius.android.MobiusAndroid;
+import com.spotify.mobius.rx2.RxMobius;
+
+import io.reactivex.ObservableTransformer;
+
+public class AddEditTaskInjector {
+
+    private AddEditTaskInjector() {}
+
+    public static MobiusLoop.Controller<AddEditTaskModel, AddEditTaskEvent> createController(
+            ObservableTransformer<AddEditTaskEffect, AddEditTaskEvent> effectHandlers,
+            AddEditTaskModel defaultModel
+    ) {
+        return MobiusAndroid.controller(createLoop(effectHandlers), defaultModel);
+    }
+
+    private static MobiusLoop.Factory<AddEditTaskModel, AddEditTaskEvent, AddEditTaskEffect> createLoop(
+            ObservableTransformer<AddEditTaskEffect, AddEditTaskEvent> effecthandlers
+    ) {
+        return RxMobius.loop(AddEditTaskLogic::update, effecthandlers)
+                .logger(AndroidLogger.tag("Add/Edit Tasks"));
+    }
+}
