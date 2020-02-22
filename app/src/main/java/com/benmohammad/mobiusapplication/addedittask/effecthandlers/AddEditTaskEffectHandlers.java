a0f3f9ce681f87c3808f2750c6f0040b23418c55
Ben Mohammad <benmohammad593@gmail.com>
2020-02-22 11:23:34 +0000

    AddEditTaskEffectHandlers added...

---
+++ app/src/main/java/com/benmohammad/mobiusapplication/addedittask/effecthandlers/AddEditTaskEffectHandlers.java
@@ -0,0 +1,73 @@
+package com.benmohammad.mobiusapplication.addedittask.effecthandlers;
+
+import android.content.Context;
+
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEffect;
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEffect.NotifyEmptyTaskNotAllowed;
+import com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEvent;
+import com.benmohammad.mobiusapplication.data.Task;
+import com.benmohammad.mobiusapplication.data.source.TasksDataSource;
+import com.benmohammad.mobiusapplication.data.source.local.TasksLocalDataSource;
+import com.benmohammad.mobiusapplication.data.source.remote.TasksRemoteDataSource;
+import com.benmohammad.mobiusapplication.util.schedulers.SchedulerProvider;
+import com.spotify.mobius.rx2.RxMobius;
+
+import java.util.UUID;
+
+import io.reactivex.ObservableTransformer;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Function;
+
+import static com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEffect.*;
+import static com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEvent.taskCreatedSuccessfully;
+import static com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEvent.taskCreationFailed;
+import static com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEvent.taskUpdateFailed;
+import static com.benmohammad.mobiusapplication.addedittask.domain.AddEditTaskEvent.taskUpdatedSuccessfully;
+import static io.reactivex.android.schedulers.AndroidSchedulers.mainThread;
+
+public class AddEditTaskEffectHandlers {
+
+    public static ObservableTransformer<AddEditTaskEffect, AddEditTaskEvent> createEfectHandlers(
+            Context context, Action showTasksList, Action showEmptyTaskError
+    ) {
+        TasksRemoteDataSource remoteSourcee = TasksRemoteDataSource.getInstance();
+        TasksLocalDataSource localource = TasksLocalDataSource.getInstance(context ,SchedulerProvider.getInstance());
+
+        return RxMobius.<AddEditTaskEffect, AddEditTaskEvent>subtypeEffectHandler()
+                .addAction(NotifyEmptyTaskNotAllowed.class, showEmptyTaskError, mainThread())
+                .addAction(Exit.class, showTasksList, mainThread())
+                .addFunction(CreateTask.class, createTaskHandler(remoteSourcee, localource))
+                .addFunction(SaveTask.class, saveTaskHandler(remoteSourcee, localource))
+                .build();
+    }
+
+    static Function<CreateTask, AddEditTaskEvent> createTaskHandler(
+            TasksDataSource remoteSource, TasksDataSource localSource
+    ) {
+        return createTaskEffect -> {
+            Task task = Task.create(UUID.randomUUID().toString(), createTaskEffect.taskDetails());
+            try {
+                remoteSource.saveTask(task);
+                localSource.saveTask(task);
+                return taskCreatedSuccessfully();
+            } catch (Exception e) {
+                return taskCreationFailed("Failed to create Task");
+            }
+        };
+    }
+
+
+    static Function<SaveTask, AddEditTaskEvent> saveTaskHandler(
+            TasksDataSource remoteSource, TasksDataSource localSource
+    ) {
+        return saveTask -> {
+            try {
+                remoteSource.saveTask(saveTask.task());
+                localSource.saveTask(saveTask.task());
+                return taskUpdatedSuccessfully();
+            } catch (Exception e) {
+                return taskUpdateFailed("Failed to update Task");
+            }
+        };
+    }
+}
